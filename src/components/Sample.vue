169c6b0c8e4d3721ea4492185b0b435b063ce4a9
HabutoShogo <habuto.shogo@gmail.com>
2020-11-26 01:02:52 +0900

    state display

---
+++ src/components/Sample.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="sample">
+    {{ str }}
+  </div>
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+  name: 'Sample',
+  computed: {
+    ...mapState({
+      str: state => state.sample.str
+    }),
+  }
+}
+</script>
+
+<style scoped></style>
